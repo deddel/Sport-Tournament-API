675cba37b34ba030533c753f98ef70c6ea64eaf0
Deddel <83078233+deddel@users.noreply.github.com>
2024-12-06 15:01:25 +0100

    Add UoW service

--- Tournament.Api/Program.cs
+++ Tournament.Api/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Tournament.Api.Extensions;
+using Tournament.Core.Repositories;
 using Tournament.Data.Data;
+using Tournament.Data.Repositories;
 
 namespace Tournament.Api
 {
@@ -25,6 +27,9 @@ namespace Tournament.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            //Add services
+            builder.Services.AddScoped<IUoW, UoW>();
 
             var app = builder.Build();
 
