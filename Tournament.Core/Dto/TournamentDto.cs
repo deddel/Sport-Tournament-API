966e01a088049efae1680aa0f39f4feee3fe1e8c
Deddel <83078233+deddel@users.noreply.github.com>
2024-12-09 10:31:36 +0100

    Added Dto's and Automapper

---
+++ Tournament.Core/Dto/TournamentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Dto
+{
+    public class TournamentDto
+    {
+        public int Id { get; init; }
+        public string? Title { get; init; }
+        public DateTime StartDate { get; init; }
+        public DateTime EndDate => StartDate.AddMonths(3);
+    }
+}
--- Tournament.Core/Entities/TournamentDetails.cs
+++ Tournament.Core/Entities/TournamentDetails.cs
@@ -9,7 +9,7 @@ namespace Tournament.Core.Entities
     public class TournamentDetails
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public DateTime StartDate { get; set; }
         //NP
         public ICollection<Game> Games { get; set; } = new List<Game>();
